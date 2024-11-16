92e9d54c7244b3829faca394db0b07b04d42ec11
ika4649 <aika.fukuoka@gmail.com>
2024-11-16 14:44:10 +0900

    ログイン後、トップページへ遷移するための記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+
+  # ログイン後のリダイレクト先をトップページに設定
+  def after_sign_in_path_for(resource)
+    top_path # トップページへのルート
+  end
 end
