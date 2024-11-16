117645c9d09aa2ce735293650927c367603df66f
ika4649 <aika.fukuoka@gmail.com>
2024-11-16 14:43:45 +0900

    新規登録後、トップページに遷移するための記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   # ログインページをルートパスに設定
   devise_scope :user do
