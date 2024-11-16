295a7efe5a1e1453c043543100db8bd515b6ce27
ika4649 <aika.fukuoka@gmail.com>
2024-11-16 12:53:23 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # ログインページをルートパスに設定
   devise_scope :user do
     root to: "devise/sessions#new"
   end
+
+  # トップページへのルート設定
+  get 'tops/index', to: 'tops#index', as: 'top'
 end
